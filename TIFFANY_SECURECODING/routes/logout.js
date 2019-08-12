60aa5bd84a50c02e699ad8e24814180132e57043
ssorrychoi <zzaesung17@gmail.com>
2019-08-12 18:02:07 +0900

    보안완료

---
+++ TIFFANY_SECURECODING/routes/logout.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    const result = {
+        msg: ""
+    };
+
+    console.log("Session ID login.js => " + req.sessionID);
+
+    if (req.session) {
+        req.session.destroy(function (err) {
+            res.redirect('/');
+        });
+    }
+});
+
+module.exports = router;
